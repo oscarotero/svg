05f3f39023a43dbe63753dbef1c803316c37dbcb
oscarotero <oom@oscarotero.com>
2015-10-10 16:53:12 +0200

    first commit. Nodejs logo

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>SVG</title>
+
+		<link rel="stylesheet" type="text/css" href="styles.dist.css">
+	</head>
+	<body>
+		<?= file_get_contents('src/nodejs/nodejs.svg'); ?>
+	</body>
+</html>
