d6e50ed433234e55edbfd09c7422f6c128eb94ef
oscarotero <oom@oscarotero.com>
2015-10-13 14:02:00 +0200

    tabs to spaces

--- index.php
+++ index.php
@@ -2,63 +2,63 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>SVG</title>
+    <head>
+        <meta charset="utf-8">
+        <title>SVG</title>
 
-		<style type="text/css">
-		<?= Svg::css() ?>
+        <style type="text/css">
+        <?= Svg::css() ?>
 
-		svg {
-			margin: 2em;
-		}
+        svg {
+            margin: 2em;
+        }
 
-		svg.is-reversed {
-			margin: 1em;
-			padding: 1em;
-		}
-		</style>
-	</head>
+        svg.is-reversed {
+            margin: 1em;
+            padding: 1em;
+        }
+        </style>
+    </head>
 
-	<body>
-		<div>
-			<?php
+    <body>
+        <div>
+            <?php
             foreach (Svg::all() as $name) {
                 if (Svg::exists($name, 'square')) {
                     echo Svg::square($name);
                 }
             }
             ?>
-		</div>
+        </div>
 
-		<div>
-			<?php
+        <div>
+            <?php
             foreach (Svg::all() as $name) {
                 if (Svg::exists($name, 'square')) {
                     echo Svg::square($name, 'is-reversed');
                 }
             }
             ?>
-		</div>
+        </div>
 
-		<div>
-			<?php
+        <div>
+            <?php
             foreach (Svg::all() as $name) {
                 if (Svg::exists($name, 'logo')) {
                     echo Svg::logo($name);
                 }
             }
             ?>
-		</div>
+        </div>
 
-		<div>
-			<?php
+        <div>
+            <?php
             foreach (Svg::all() as $name) {
                 if (Svg::exists($name, 'logo')) {
                     echo Svg::logo($name, 'is-reversed');
                 }
             }
             ?>
-		</div>
-	</body>
+        </div>
+    </body>
 </html>
